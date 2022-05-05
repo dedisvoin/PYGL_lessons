1ba2cc79cc79f7ae138bf20accd8a958cc0cc1e1
dedisvoin <88434293+dedisvoin@users.noreply.github.com>
2022-05-05 21:06:31 +0300

    Update create window.py

--- create window.py
+++ create window.py
@@ -4,3 +4,5 @@ win = GL.Display_init_() # creating a window instance
 
 while win.CEU(): # rendering cycle
     pass
+
+# when creating a window, you can specify the flags parameter on GL.D_full and then the window will be created in full-screen mode
